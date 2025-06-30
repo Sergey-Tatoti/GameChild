a54d3159e59b282e1f55930ea0ba4d80e212cb64
OlegPuzo <134072583+OlegPuzo@users.noreply.github.com>
2025-06-30 22:09:52 +0300

    Визуал рекламы

---
+++ Source/5. Scripts/3.Managers/3.Menu/ADScontroller.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ADScontroller : MonoBehaviour
+{
+    [SerializeField] private Button _adsButtonMenu;
+    [SerializeField] private Button _adsButtonMap;
+    [Header("Меню с кнопкой покупки")]
+    [SerializeField] private GameObject _adsBuyPanel;
+    [SerializeField] private Button _adsBuyButton;
+    [SerializeField] private Button _adsReturnButton;
+    [Header("Меню с проверкой")]
+    [SerializeField] private GameObject _adsCheckPanel;
+    [SerializeField] private Button _adsCheckButtonBuy;
+    [SerializeField] private Button _adsCheckButtonReturn;
+}
