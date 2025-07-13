7b1b554a60b2553d5bb6f13175a1f9023776e601
OlegPuzo <134072583+OlegPuzo@users.noreply.github.com>
2025-07-13 20:45:37 +0300

    Уровни
    Осталось 8 штук + первично протестить

--- Source/5. Scripts/3.Managers/3.Menu/ADScontroller.cs
+++ Source/5. Scripts/3.Managers/3.Menu/ADScontroller.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -15,4 +16,44 @@ public class ADScontroller : MonoBehaviour
     [SerializeField] private GameObject _adsCheckPanel;
     [SerializeField] private Button _adsCheckButtonBuy;
     [SerializeField] private Button _adsCheckButtonReturn;
+    [SerializeField] private TMP_InputField _adsInputField;
+    [SerializeField] private string _resultCheck;
+
+    private void OnEnable()
+    {
+        _adsButtonMenu.onClick.AddListener(() => ShowAdsBuyPanel(true));
+        _adsButtonMap.onClick.AddListener(() => ShowAdsBuyPanel(true));
+        _adsBuyButton.onClick.AddListener(AttemptBuyAds);
+        _adsReturnButton.onClick.AddListener(() => ShowAdsBuyPanel(false));
+        _adsCheckButtonBuy.onClick.AddListener(() => ShowCheckPanel(true));
+        _adsCheckButtonReturn.onClick.AddListener(() => ShowCheckPanel(false));
+    }
+
+    private void OnDisable()
+    {
+        _adsButtonMenu.onClick.RemoveListener(() => ShowAdsBuyPanel(true));
+        _adsButtonMap.onClick.RemoveListener(() => ShowAdsBuyPanel(true));
+        _adsBuyButton.onClick.RemoveListener(AttemptBuyAds);
+        _adsReturnButton.onClick.RemoveListener(() => ShowAdsBuyPanel(false));
+        _adsCheckButtonBuy.onClick.RemoveListener(AttemptBuyAds);
+        _adsCheckButtonReturn.onClick.RemoveListener(() => ShowCheckPanel(false));
+    }
+
+    private void ShowAdsBuyPanel(bool isShow)
+    {
+        _adsBuyPanel.SetActive(isShow);
+    }
+
+    private void ShowCheckPanel(bool isShow)
+    {
+        _adsCheckPanel.SetActive(isShow);
+    }
+
+    private void AttemptBuyAds()
+    {
+        if (_resultCheck == _adsInputField.text)
+        {
+
+        }    
+    }
 }
