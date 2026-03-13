9b9b69da8518dd7cec2940ee4ab323d597ddd067
Sergey-Dusaev <68469332+Sergey-Tatoti@users.noreply.github.com>
2026-03-14 01:05:21 +0300

    Update

--- Source/5. Scripts/3.Managers/3.Menu/ADScontroller.cs
+++ Source/5. Scripts/3.Managers/3.Menu/ADScontroller.cs
@@ -1,6 +1,4 @@
 ﻿using GamePush;
-using System.Collections;
-using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.Events;
@@ -15,12 +13,6 @@ public class ADScontroller : MonoBehaviour
     [SerializeField] private Button _adsBuyButton;
     [SerializeField] private Button _adsReturnButton;
     [SerializeField] private TMP_Text _textPrice;
-    [Header("Меню с проверкой")]
-    [SerializeField] private GameObject _adsCheckPanel;
-    [SerializeField] private Button _adsCheckButtonBuy;
-    [SerializeField] private Button _adsCheckButtonReturn;
-    [SerializeField] private TMP_InputField _adsInputField;
-    [SerializeField] private string _resultCheck;
 
     private SoundManager _soundManager;
 
@@ -31,10 +23,7 @@ public class ADScontroller : MonoBehaviour
         _adsButtonMenu.onClick.AddListener(() => ShowAdsBuyPanel(true));
         _adsButtonMap.onClick.AddListener(() => ShowAdsBuyPanel(true));
         _adsBuyButton.onClick.AddListener(AttemptBuyAds);
-
         _adsReturnButton.onClick.AddListener(() => ShowAdsBuyPanel(false));
-        _adsCheckButtonBuy.onClick.AddListener(AttemptBuyAds);
-        _adsCheckButtonReturn.onClick.AddListener(() => ShowCheckPanel(false));
     }
 
     private void OnDisable()
@@ -42,10 +31,7 @@ public class ADScontroller : MonoBehaviour
         _adsButtonMenu.onClick.RemoveListener(() => ShowAdsBuyPanel(true));
         _adsButtonMap.onClick.RemoveListener(() => ShowAdsBuyPanel(true));
         _adsBuyButton.onClick.RemoveListener(AttemptBuyAds);
-
         _adsReturnButton.onClick.RemoveListener(() => ShowAdsBuyPanel(false));
-        _adsCheckButtonBuy.onClick.RemoveListener(AttemptBuyAds);
-        _adsCheckButtonReturn.onClick.RemoveListener(() => ShowCheckPanel(false));
     }
 
     public void SetBaseValues(SoundManager soundManager) => _soundManager = soundManager;
@@ -64,7 +50,6 @@ public class ADScontroller : MonoBehaviour
         _adsButtonMap.gameObject.SetActive(!isBuyAds);
 
         _adsBuyPanel.gameObject.SetActive(false);
-        _adsCheckPanel.gameObject.SetActive(false);
     }
 
     private void ShowAdsBuyPanel(bool isShow)
@@ -73,19 +58,9 @@ public class ADScontroller : MonoBehaviour
         _adsBuyPanel.SetActive(isShow);
     }
 
-    private void ShowCheckPanel(bool isShow)
-    {
-        _soundManager.PlaySound(SoundManager.TypeSound.ClickButton);
-        _adsCheckPanel.SetActive(isShow);
-    }
-
     private void AttemptBuyAds()
     {
         _soundManager.PlaySound(SoundManager.TypeSound.ClickButton);
-
-        if (_resultCheck == _adsInputField.text)
-        {
-            ClickedBuyAds?.Invoke();
-        }
+        ClickedBuyAds?.Invoke();
     }
 }
